b6e21047430c801f9ec6fa2e5e08d07c39008397
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-04-30 12:25:44 +0200

    test

---
+++ CarrascalFrancoJoaquin-ProyectoFinal/src/main/java/com/salesianostriana/dam/carrascalfrancojoaquinproyectofinal/CarrascalFrancoJoaquinProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.carrascalfrancojoaquinproyectofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrascalFrancoJoaquinProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrascalFrancoJoaquinProyectoFinalApplication.class, args);
+	}
+
+}
---
+++ CarrascalFrancoJoaquin-ProyectoFinal/src/test/java/com/salesianostriana/dam/carrascalfrancojoaquinproyectofinal/CarrascalFrancoJoaquinProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.carrascalfrancojoaquinproyectofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrascalFrancoJoaquinProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
