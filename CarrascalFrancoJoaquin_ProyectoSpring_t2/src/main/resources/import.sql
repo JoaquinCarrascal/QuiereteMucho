20cea7feb63bf968224b6e8579e132ca52bf6ed2
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-05-06 19:02:47 +0200

    agregar productos con url de imagen completo

--- CarrascalFrancoJoaquin_ProyectoSpring_t2/src/main/resources/import.sql
+++ CarrascalFrancoJoaquin_ProyectoSpring_t2/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 insert into product (id, name, description, avg_duration, base_price , product_type , desc_img) values (1, 'Mechas californianas', 'Inspiradas en las surfistas de las playas de California, pretenden emular el efecto que la constante exposición al sol dejaba sobre su pelo.', 60,35,'PELUQUERIA' , 'https://i.imgur.com/3tZkQxM.jpeg');
-insert into product (id, name, description, avg_duration, base_price , product_type) values (2, 'Tinte mujer', 'Tinte para mujer + tratamiento suavizante, el color elegido es ajeno al precio.', 45 , 28 ,'PELUQUERIA');
-insert into product (id, name, description, avg_duration, base_price , product_type) values (3, 'Manicura francesa', 'Un clásico de la belleza que nunca pasa de moda, además incluye tratamiento de la cutícula.', 25 ,15.5,'ESTETICA');
-insert into product (id, name, description, avg_duration, base_price , product_type) values (4, 'Manicura de porcelana', 'Tratamiento hidratante sobre la uña + manicura de polvo de porcelana de la máxima calidad.', 30 ,18 ,'ESTETICA');
+insert into product (id, name, description, avg_duration, base_price , product_type , desc_img) values (2, 'Tinte mujer', 'Tinte para mujer + tratamiento suavizante, el color elegido es ajeno al precio.', 45 , 28 ,'PELUQUERIA' , 'https://i.imgur.com/Pde5TXa.jpeg');
+insert into product (id, name, description, avg_duration, base_price , product_type , desc_img) values (3, 'Manicura francesa', 'Un clásico de la belleza que nunca pasa de moda, además incluye tratamiento de la cutícula.', 25 ,15.5,'ESTETICA' , 'https://i.imgur.com/IalSjXO.jpeg');
+insert into product (id, name, description, avg_duration, base_price , product_type , desc_img) values (4, 'Manicura de porcelana', 'Tratamiento hidratante sobre la uña + manicura de polvo de porcelana de la máxima calidad.', 30 ,18 ,'ESTETICA' , 'https://i.imgur.com/RAb71tI.jpeg');
 
 ALTER SEQUENCE product_seq RESTART WITH 54;
 
